9d6e88496f944c7e10ac0276df83732c1061dbe0
Neil Copland <neil.copland@gmail.com>
2022-08-21 18:18:17 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,7 +50,8 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
-        'django_celery_results'
+        'django_celery_results',
+        'django_celery_beat',
     ]
 
     CELERY_RESULT_BACKEND = "django-db"
