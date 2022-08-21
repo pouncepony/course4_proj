c0f44a2768185afba582efd48c9027e265a2a474
Neil Copland <neil.copland@gmail.com>
2022-08-21 11:08:55 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -55,6 +55,9 @@ class Dev(Configuration):
 
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
     
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
